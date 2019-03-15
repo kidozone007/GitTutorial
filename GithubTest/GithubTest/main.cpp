3ccdac97967c509f605ef867ac3fef9337739df1
DESKTOP-OA1TEAJ\mike <miikenick456@gmail.com>
2019-03-15 12:03:17 +0800

    practice8

--- GithubTest/GithubTest/main.cpp
+++ GithubTest/GithubTest/main.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 int main()
 {
-<<<<<<< HEAD
 	cout << "Hello";
 	cout << " World!!!";
 }
@@ -12,10 +11,10 @@ void fuck()
 {
 	cout << "Hello";
 	cout << " World!!!";
-=======
 	cout << "practice 5";
 	cout << " practice 6";
 	cout << "practice 7";
 	cout << "practicetest";
->>>>>>> 64c10604528b9ca8337be6e783775a5256771a8b
+	cout << "practice8";
+
 }
