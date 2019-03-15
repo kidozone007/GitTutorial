8708cf9134f8b55f5fee07762ccc3ff53f6ac7d3
DESKTOP-OA1TEAJ\mike <miikenick456@gmail.com>
2019-03-15 11:54:55 +0800

    test

---
+++ GithubTest/GithubTest/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	cout << "Hello";
+	cout << " World!!!";
+}
+
+void fuck()
+{
+	cout << "Hello";
+	cout << " World!!!";
+	cout << "practice 5";
+	cout << " practice 6";
+	cout << "practice 7";
+	
+}
