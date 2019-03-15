b6771f0e7b1e77224bd7c5d1708a071ec3248d2f
105590014 <kidozone007@gmail.com>
2019-03-15 11:59:13 +0800

    test

--- GithubTest/GithubTest/main.cpp
+++ GithubTest/GithubTest/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int main()
 {
+<<<<<<< HEAD
 	cout << "Hello";
 	cout << " World!!!";
 }
@@ -11,8 +12,10 @@ void fuck()
 {
 	cout << "Hello";
 	cout << " World!!!";
+=======
 	cout << "practice 5";
 	cout << " practice 6";
 	cout << "practice 7";
-	
+	cout << "practicetest";
+>>>>>>> 64c10604528b9ca8337be6e783775a5256771a8b
 }
